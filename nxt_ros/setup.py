d7b88b210cdbc6dc8b4d98628e3df49c9ef8f979
Nick Chalko <nick@chalko.com>
2014-01-04 17:45:29 -0800

    copy setup

--- nxt_ros/setup.py
+++ nxt_ros/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['nxt'],
+    packages=['nxt_ros'],
     package_dir={'': '.'}
 )
 
